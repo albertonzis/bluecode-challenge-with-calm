3f471aa70048cfadfc63084838318c6d7d9d7d43
albertonzis <luizalberton@gmail.com>
2023-09-09 11:10:44 -0300

    base user repository

---
+++ src/main/java/bluecode/tech/interview/controllers/UserController.java
@@ -0,0 +1,38 @@
+package bluecode.tech.interview.controllers;
+
+import bluecode.tech.interview.dtos.UserDTO;
+import bluecode.tech.interview.models.User;
+import bluecode.tech.interview.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Long> createUser(User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, UserDTO user) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
